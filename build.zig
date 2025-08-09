145a8476654e9ed1162756769aadf8614ae460d6
The One The Master <vacpox@gmail.com>
2025-08-10 01:19:12 +0300

    dedup symbol name

--- build.zig
+++ build.zig
@@ -10,8 +10,22 @@ pub fn build(b: *std.Build) void {
     });
     const asm_object_dir = b.makeTempPath();
 
+    var key: u64 = undefined;
+    std.crypto.random.bytes(@as([*]u8, @ptrCast(&key))[0..8]);
+    var prng = std.Random.DefaultPrng.init(key);
+    var random = prng.random();
+    const random_number = random.intRangeAtMost(u64, 0x10000000, 0xffffffff);
+    const asm_string_object_name = std.fmt.allocPrint(
+        std.heap.page_allocator,
+        "{x}.o",
+        .{random_number},
+    ) catch @panic("OOM");
+
     const asm_object_file =
-        b.pathJoin(&[_][]const u8{ asm_object_dir, "syscall_wrapper.o" });
+        b.pathJoin(&[_][]const u8{
+            asm_object_dir,
+            asm_string_object_name,
+        });
 
     const asm_file_path =
         b.path("src/syscall_wrapper.asm").getPath3(b, null).toString(std.heap.page_allocator) catch {
